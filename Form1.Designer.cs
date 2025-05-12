4f2f2862218895a73d85eb9ff13b08ae99840beb
Valeria Bernal Salcedo <vabersal05@gmail.com>
2025-05-12 08:57:49 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PRACTICA_CODIFICACIONES_DIGITALES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.signal = new System.Windows.Forms.TextBox();
+            this.GRAFICAR = new System.Windows.Forms.Button();
+            this.pbNRZL = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNRZL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SEÑAL A GRAFICAR:";
+            // 
+            // signal
+            // 
+            this.signal.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signal.Location = new System.Drawing.Point(458, 50);
+            this.signal.Name = "signal";
+            this.signal.Size = new System.Drawing.Size(290, 35);
+            this.signal.TabIndex = 1;
+            // 
+            // GRAFICAR
+            // 
+            this.GRAFICAR.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GRAFICAR.Location = new System.Drawing.Point(855, 53);
+            this.GRAFICAR.Name = "GRAFICAR";
+            this.GRAFICAR.Size = new System.Drawing.Size(277, 58);
+            this.GRAFICAR.TabIndex = 2;
+            this.GRAFICAR.Text = "GRAFICAR";
+            this.GRAFICAR.UseVisualStyleBackColor = true;
+            this.GRAFICAR.Click += new System.EventHandler(this.GRAFICAR_Click);
+            // 
+            // pbNRZL
+            // 
+            this.pbNRZL.Location = new System.Drawing.Point(59, 123);
+            this.pbNRZL.Name = "pbNRZL";
+            this.pbNRZL.Size = new System.Drawing.Size(1137, 196);
+            this.pbNRZL.TabIndex = 3;
+            this.pbNRZL.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 717);
+            this.Controls.Add(this.pbNRZL);
+            this.Controls.Add(this.GRAFICAR);
+            this.Controls.Add(this.signal);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbNRZL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox signal;
+        private System.Windows.Forms.Button GRAFICAR;
+        private System.Windows.Forms.PictureBox pbNRZL;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PRACTICA_CODIFICACIONES_DIGITALES
+{
+    public partial class Form1 : Form
+    {
+        string bits;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GRAFICAR_Click(object sender, EventArgs e)
+        {
+            bits = signal.Text;
+
+            if(!bits.All(c => c == '0' || c == '1')){
+                MessageBox.Show("Solo se permiten 0 y 1.");
+                return;
+            }
+
+            pbNRZL.Paint += DrawNRZL;
+            pbNRZL.Refresh();
+
+        }
+
+        private void DrawNRZL(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // 'Lienzo' para dibujar
+            Pen pen = new Pen(Color.Blue, 2); // 'Lapiz' azul para la señal
+            Pen gridPen = new Pen(Color.LightGray, 1); // 'Lapiz' gris para la cuadricula
+            Font font = new Font("Arial", 10); // Fuente
+
+            // Coordenadas base
+            int xStart = 50; // Donde empieza el dibujo en X
+            int yZero = 150; // Coordenada vertical para el nivel 0
+            int yOne = 50; // Coordenada vertical para el nivel 1
+            int bitWidth = 50; // Ancho que ocupa 1 bit
+
+            // Etiquetas '0' y '1' en el eje Y
+            g.DrawString("1", font, Brushes.Black, 20, yOne - 10);
+            g.DrawString("0", font, Brushes.Black, 20, yZero - 10);
+
+            // Dibujar líneas verticales de la cuadrícula (1 por cada bit)
+            for (int i = 0; i <= bits.Length; i++)
+            {
+                int x1 = xStart + i * bitWidth;
+                g.DrawLine(gridPen, x1, yOne, x1, yZero);
+            }
+
+            // Dibujar líneas horizontales (nivel 1 y 0)
+            g.DrawLine(gridPen, xStart, yOne, xStart + bits.Length * bitWidth, yOne);
+            g.DrawLine(gridPen, xStart, yZero, xStart + bits.Length * bitWidth, yZero);
+
+            // Dibujar los bits
+            int currentY = bits[0] == '1' ? yOne : yZero; // Nivel inicial
+            int x = xStart;
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                int nextY = bits[i] == '1' ? yOne : yZero;
+
+                // Dibujar bit encima
+                g.DrawString(bits[i].ToString(), font, Brushes.Black, x + 15, yZero + 10);
+
+                // Si hay cambio de nivel, dibujar línea vertical
+                if (i > 0 && nextY != currentY)
+                {
+                    g.DrawLine(pen, x, currentY, x, nextY);
+                }
+
+                // Dibujar línea horizontal para el bit
+                g.DrawLine(pen, x, nextY, x + bitWidth, nextY);
+                x += bitWidth;
+                currentY = nextY;
+            }
+
+            // Dibujar flecha final
+            g.DrawLine(Pens.Blue, x, currentY, x + 10, currentY);
+            g.DrawLine(Pens.Blue, x + 5, currentY - 5, x + 10, currentY);
+            g.DrawLine(Pens.Blue, x + 5, currentY + 5, x + 10, currentY);
+        }
+    }
+}
